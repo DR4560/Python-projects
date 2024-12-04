f463a1fe188824be7db9d85dd0bf7c55f3e06b19
DR4560 <elizavetadudaeva1@gmail.com>
2024-12-05 00:32:04 +0300

    Update main.py

--- Pandas/main.py
+++ Pandas/main.py
@@ -1,1 +1,34 @@
+#This is a real coding samples via "Inspection Profiles.csv" task
+
+import pandas as pd
+import sys
+print(sys.version)
+print('pd vs', pd.__version__)
+
+pd.DataFrame()#spreetshit
+
+#Example
+#Data for plotting
+t = np.arange(0.0 , 2.0 , 0.01)
+s = 1 + np.sin(2* np.pi * t)
+
+fig, ax = plt.subplots()
+ax.plot(t, s)
+
+ax.set(xlabel = 'time (s)', ylabel = 'voltage(mV)', title = 'Yeah I copied that data lol')
+ax.grid()
+
+
+
+import numpy as np
+import matplotlib.pylab as plt
+import matplotlib
+
+import pandas as pd
+import sys
+print(sys.version)
+print('pd vs', pd.__version__)
+
+pd.DataFrame()#spreetshit
+
 
